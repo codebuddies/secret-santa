fe34c53240b6c69c05d69ef670f98995b877afa0
distalx <distalx@gmail.com>
2016-11-08 21:42:44 +0530

    refactor(gift): relocated gift template

--- imports/template/gift/index.js
+++ imports/template/gift/index.js
@@ -2,3 +2,5 @@ import './my_gift.js';
 import './sent_gift_modal.js';
 import './received_gift_modal.js';
 import './gift_received.html';
+import './gift_arrived.js';
+import './gift_yet_to_be_arrived.html';
