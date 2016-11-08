1a2d55400c47ee9d9a1c0277e39e1a466a9f6920
distalx <distalx@gmail.com>
2016-11-08 21:07:08 +0530

    fix(letter): in profile
    user can view his letter on their profile page

--- imports/template/profile/profile.js
+++ imports/template/profile/profile.js
@@ -1,18 +1,18 @@
 import './profile.html';
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating'
-import { Questionnaires } from '../../api/questionnaires/questionnaires.js';
+import { Letters } from '../../api/letters/letters.js';
 
 Template.profile.onCreated(function(){
   this.subscribe("userInformationById", FlowRouter.getParam('userId'));
-  this.subscribe("questionnaireByUserId", FlowRouter.getParam('userId'));
+  this.subscribe("myLetter", FlowRouter.getParam('userId'));
 });
 
 Template.profile.helpers({
   userInfo: function(){
     return Meteor.users.findOne({'_id':FlowRouter.getParam('userId')});
   },
-  questionnaire: function(){
-    return Questionnaires.findOne({'user.id':FlowRouter.getParam('userId')});
+  letter: function(){
+    return Letters.findOne({'user.id':FlowRouter.getParam('userId')});
   }
 })
