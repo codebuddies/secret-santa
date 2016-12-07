96b9396d45fdc2f34cdaa57d1db3ec6922ee4455
distalx <distalx@gmail.com>
2016-12-07 17:25:46 +0530

    chore(cron): cron will run after 11'th dec 2016
    cron job will only run if the current date is > 11th dec 2016 10:10 AM or test mode is TRUE

--- server/main.js
+++ server/main.js
@@ -13,5 +13,8 @@ Meteor.startup(() => {
 
   process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
 
-  SyncedCron.start();
+  if ( Date.now() > new Date('December 11, 2016 10:10:10') || Meteor.settings.private.testMode) {
+    SyncedCron.start();
+  }
+
 });
